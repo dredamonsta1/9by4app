8a6dcc6249b92f50ca59defb80ae77b4c382bb15
andre <andre_w3@yahoo.com>
2020-09-14 00:38:41 -0400

    decoupleing components

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import './App.css';
 // import Rapper from './components/Rappers';
 // import image from './image';
 // import style from './'
-import { Rappers } from '../components/RapperList';
+import  NavBar  from './components/NavBar/NavBar';
+import { Rappers } from './components/RapperList';
 
 
 function App() {
   return (
 
-    <div className = "App" >
-    <header className = "App-header" >
-    <h1>
-    <span>9</span>
-    <span>X</span>
-    <span>4</span>
-    
-    </h1>
+    <div className = "app" >
+ 
 
-    </header>
-
-    
+    <NavBar/>
     <Rappers/>
 
     </div>
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const NavBar = () => {
+
+    return (
+        <header className = "App-header" >
+    <h1 className = "nav-name">
+    <span>9</span>
+    <span>X</span>
+    <span>4</span>
+    
+    </h1>
+
+    </header>
+    )
+}
+
+
+export default NavBar;
