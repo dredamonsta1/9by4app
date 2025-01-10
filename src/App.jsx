088fa68d6b465d9c13a07783c9bf288e3c0fd4f0
Andre Wilkinson <andre_w3@yahoo.com>
2025-01-09 22:39:54 -0500

    fetching api

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // import Rapper from './components/Rappers';
 // import image from './image';
-import RapperCloutButton from './RapperCloutButton';
-import Rappers from './components/RapperList';
-import ClickableList from './components/RapperList';
+import RapperCloutButton from "./RapperCloutButton";
+import Rappers from "./components/RapperList";
+import ClickableList from "./components/RapperList";
 // import MainList from './components/MainList';
-
 
 function App() {
   return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1> Pass Da Aux </h1>
+          {/* <MainList/> */}
+          {/* {item.album} */}
+        </header>
 
-        <>
-    <div className = "App" >
-    <header className = "App-header" >
-        <h1 > Pass Da Aux </h1>
-        {/* <MainList/> */}
-        {/* {item.album} */}
-
-    </header>
-
-    
-      <Rappers />
-        <ClickableList />
+        <Rappers />
+        {/* <ClickableList /> */}
         {/* <RapperCloutButton /> */}
-        
-
-    </div>
-        </>
+      </div>
+    </>
   );
 }
 
--- src/components/RapperList.jsx
+++ src/components/RapperList.jsx
@@ -1,33 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 // import {RapperCloutButton} from '../RapperCloutButton';
 
-const ClickableList = props => {
+const ClickableList = (props) => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [currentFetch, setCurrentFetch] = useState({
+    artist_id: 0,
+    name: "",
+    genre: "",
+    count: 0,
+    state: "",
+    region: "",
+    label: "",
+    mixtape: "",
+    album: "",
+    year: 0,
+    certifications: "",
+  });
   // Initialize the list with an array of objects containing strings and a count of 0
   useEffect(() => {
-    // async function fetchData() {
-    //   try {
-    //     const response = await axios.get('https://ninebyfourapi.herokuapp.com/api', {method: 'GET'});
-    //     setItems(response.data);
-    //     setLoading(false);
-    //     console.log(response.data);
-    //   } catch (error) {
-    //     setError(true);
-    //     setLoading(false);
-    //   }
-    // }
-
-    axios.get('https://ninebyfourapi.herokuapp.com/api', {method: 'GET'})
-    .then((res) => {
-      setItems(res.data);
-      console.log(res.data);
-      console.log(error);
-      
-    })
-  }, [])
+    axios
+      .get("https://ninebyfourapi.herokuapp.com/api", { method: "GET" })
+      .then((res) => {
+        const {
+          artist_id,
+          name,
+          genre,
+          count,
+          state,
+          region,
+          label,
+          mixtape,
+          album,
+          year,
+          certifications,
+        } = res.data;
+        setCurrentFetch({
+          ...currentFetch,
+          ...{
+            artist_id,
+            name,
+            genre,
+            count,
+            state,
+            region,
+            label,
+            mixtape,
+            album,
+            year,
+            certifications,
+          },
+        });
+        // console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   // const [R, setR] = useState([
   //   { id: 1, artist_name: "Kendrick Lamar", aka: "KDot", genre: "Hip Hop", count: 0, state: "CA", region: "west", label: "TDE", album: "Mr. Moral & the Big Stepper", year: 2023, certifications:"platinum" },
   //   { id: 2, artist_name: "Jay-Z", genre: "Hip Hop", count: 0 },
@@ -42,11 +73,11 @@ const ClickableList = props => {
 
   // Handle button click
   const handleClick = (index) => {
-      // Create a new array with updated count for the clicked item
-      const newItems = [...items];
-      newItems[index].count += 1;
-      
-      console.log(items);
+    // Create a new array with updated count for the clicked item
+    const newItems = [...currentFetch];
+    newItems[index].count += 1;
+
+    console.log(newItems);
     // Sort items based on count in descending order
     newItems.sort((a, b) => b.count - a.count);
 
@@ -59,9 +90,10 @@ const ClickableList = props => {
       <h2>Pass Da Aux</h2>
       <ul>
         {Object.keys(items).map((item, index, artist_id) => (
-          <li key={item.artist_id} style={{ marginBottom: '10px' }}>
+          <li key={artist_id} style={{ marginBottom: "10px" }}>
             <button onClick={() => handleClick(index)}>
-              {item.name} Clout: {item.count} {item.genre} {item.album} {item.year}
+              {item.name} Clout: {item.count} {item.genre} {item.album}{" "}
+              {item.year}
             </button>
           </li>
         ))}
