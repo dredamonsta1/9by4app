60dca5d41854989ebfb798e04149cc9308cbdbd2
Andre Wilkinson <andre_w3@yahoo.com>
2025-01-24 14:53:50 -0500

    getting data and clout button isnt breaking

--- src/components/RapperList.jsx
+++ src/components/RapperList.jsx
@@ -21,17 +21,6 @@ const ClickableList = (props) => {
         console.log(error);
       });
   }, []);
-  // const [R, setR] = useState([
-  //   { id: 1, artist_name: "Kendrick Lamar", aka: "KDot", genre: "Hip Hop", count: 0, state: "CA", region: "west", label: "TDE", album: "Mr. Moral & the Big Stepper", year: 2023, certifications:"platinum" },
-  //   { id: 2, artist_name: "Jay-Z", genre: "Hip Hop", count: 0 },
-  //   { id: 3, artist_name: "Drake", genre: "Hip Hop", count: 0 },
-  //   { id: 4, artist_name: "J Cole", genre: "Hip Hop", count: 0 },
-  //   { id: 5, artist_name: "Pusha T", genre: "Hip Hop", count: 0 },
-  //   { id: 6, artist_name: "Young Thug", genre: "Hip Hop", count: 0 },
-  //   { id: 7, artist_name: "ScHoolboyQ", genre: "Hip Hop", count: 0 },
-  //   { id: 8, artist_name: "Kanye West", genre: "Hip Hop", count: 0 },
-  //   { id: 9, artist_name: "Cardi B", genre: "Hip Hop", count: 0 }
-  // ]);
 
   // Handle button click
   const handleClick = (index) => {
