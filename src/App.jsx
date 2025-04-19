2951fc7a5ecb20cffb0d3504fa088df2fc1a1b0f
Andre Wilkinson <andre_w3@yahoo.com>
2025-04-19 02:50:20 -0400

    routes work lessss gooooo

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
           {/* <Route path="/login" element={<Login />} /> */}
           <Route path="/signup" element={<AuthForm />} />
-          {/* <Route path="/profile" element={<UserProfile />} /> */}
+          <Route path="/profile" element={<UserProfile />} />
           {/* <Route path="/rappers" element={<Rappers />} /> */}
           {/* <Route path="/rappers/:id" element={<RapperDetails />} /> */}
           {/* <Route path="/" element={<MainList />} /> */}
