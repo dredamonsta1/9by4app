7268ad954229202e63d971c800b8bd59f0f609fe
Andre Wilkinson <andre_w3@yahoo.com>
2025-04-18 20:37:27 -0400

    setting up browser router

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,25 @@ import AuthForm from "./components/Signup/Signup";
 // import ClickableList from "./components/RapperList";
 import MainList from "./components/MainList/MainList";
 import UserProfile from "./components/userProfile/UserProfile";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <UserProfile />
-      <div className="App">
-        <header className="App-header">
-          <h1> Pass Da Aux </h1>
-          <MainList />
-          <AuthForm />
-        </header>
+      <BrowserRouter>
+        <UserProfile />
+        <div className="App">
+          <header className="App-header">
+            <h1> Pass Da Aux </h1>
+            <MainList />
+            <AuthForm />
+          </header>
 
-        {/* <Rappers /> */}
-        {/* <ClickableList /> */}
-        {/* <RapperCloutButton /> */}
-      </div>
+          {/* <Rappers /> */}
+          {/* <ClickableList /> */}
+          {/* <RapperCloutButton /> */}
+        </div>
+      </BrowserRouter>
     </>
   );
 }
