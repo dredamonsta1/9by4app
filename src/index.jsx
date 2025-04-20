2bdf3590728e7ec3de3f90637a487ccc06b44922
Andre Wilkinson <andre_w3@yahoo.com>
2025-04-20 11:53:08 -0400

    install redux toolkit

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+// import { Provider } from "mobx-react";
+import store from "./store/store";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(
+  <Provider store={store}>
+    <App tab="home" />
+  </Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
