ef5bdffd1fefbe06faac7cbaadd5fba1252b9649
Andre Wilkinson <andre_w3@yahoo.com>
2025-05-23 18:57:07 -0400

    user profile component refactor

--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -1,63 +1,100 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 // import MainList from "../MainList/MainList";
+import axiosInstance from "../../utils/axiosInstance";
 import RapperList from "../RapperList";
 
 const UserProfile = (props) => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
-    axios
-      .get("https://ninebyfourapi.herokuapp.com/api/users", {
-        method: "GET",
-      })
-      .then((res) => {
-        setUser(res.data.users);
+    const fetchUserProfile = async () => {
+      try {
+        const response = await axiosInstance.get("/users");
+        setUser(response.data.users);
         setLoading(false);
-        console.log("Users:", res.data.users);
-      })
-      .catch((error) => {
-        console.error("fetching data:", error);
-        setError(true);
+      } catch (error) {
+        console.error(
+          "Error fetching user profile:",
+          error.response?.data || error.message
+        );
+        setError(error.response?.data?.message || "failed to fetch user data.");
+
         setLoading(false);
-        if (error.res) {
-          console.log("Error response:", error.res.data);
-        } else if (error.req) {
-          console.log("Request error:", error.req);
-        } else {
-          console.log("General error:", error.message);
-        }
-        console.log("Name", user.username);
-      });
+      }
+    };
+    fetchUserProfile();
   }, []);
+  //   setLoading(true);
+  //   axios
+  //     .get("https://ninebyfourapi.herokuapp.com/api/users", {
+  //       method: "GET",
+  //     })
+  //     .then((res) => {
+  //       setUser(res.data.users);
+  //       setLoading(false);
+  //       console.log("Users:", res.data.users);
+  //     })
+  //     .catch((error) => {
+  //       console.error("fetching data:", error);
+  //       setError(true);
+  //       setLoading(false);
+  //       if (error.res) {
+  //         console.log("Error response:", error.res.data);
+  //       } else if (error.req) {
+  //         console.log("Request error:", error.req);
+  //       } else {
+  //         console.log("General error:", error.message);
+  //       }
+  //       console.log("Name", user.username);
+  //     });
+  // }, []);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error fetching data</p>;
+  if (error) {
+    return <p>Error {error}fetching data</p>;
+  }
+  if (!user) {
+    return <div>Loading user profile.....</div>;
+  }
 
   return (
     <div>
-      <h1> user Profile </h1>
-      {Array.isArray(user) ? (
-        user.map((user) => (
-          <div key={user.id}>
-            <h2>Username: {user.username || "N/A"}</h2>
-            <p>Email: {user.email || "N/A"}</p>
-            <p>Role: {user.role || "N/A"}</p>
-          </div>
-        ))
+      <h3> user Profile </h3>
+      {user.length > 0 ? (
+        <ul>
+          {user.map((user) => (
+            <li key={user.user_id}>
+              <p>Username: {user.username}</p>
+              <p>Email: {user.email}</p>
+              <p>Role: {user.role}</p>
+            </li>
+          ))}
+        </ul>
       ) : (
-        <div>
-          <h2>Username: {user.username || "N/A"}</h2>
-          <p>Email: {user.email || "N/A"}</p>
-          <p>Role: {user.role || "N/A"}</p>
-        </div>
+        <p>No user data found</p>
       )}
-      {/* <MainList /> */}
-      <RapperList />
+      {/* <RapperList /> */}
     </div>
+    // {Array.isArray(user) ? (
+    //   user.map((user) => (
+    //     <div key={user.id}>
+    //       <h2>Username: {user.username || "N/A"}</h2>
+    //       <p>Email: {user.email || "N/A"}</p>
+    //       <p>Role: {user.role || "N/A"}</p>
+    //     </div>
+    //   ))
+    // ) : (
+    //   <div>
+    //     <h2>Username: {user.username || "N/A"}</h2>
+    //     <p>Email: {user.email || "N/A"}</p>
+    //     <p>Role: {user.role || "N/A"}</p>
+    // </div>
+    // )}
+    // {/* <MainList /> */}
+    // </div>
   );
 };
 export default UserProfile;
