d9ddcecf82646fdc4f4af75081069d2e3e3c313c
Andre Wilkinson <andre_w3@yahoo.com>
2025-12-30 16:51:02 -0500

    code cleanup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,4 @@
 // src/redux/store.js
-// import { configureStore } from "@reduxjs/toolkit";
-// import artistsReducer from "./reducers/artistsReducer";
-// import authReducer from "../store/authSlice"; // Import the auth slice/reducer
-// import profileListReducer from "./profileListSlice"; // 1. Import the new reducer
-
-// const store = configureStore({
-//   reducer: {
-//     artists: artistsReducer,
-//     auth: authReducer,
-//     profileList: profileListReducer, // 2. Add the new reducer to the store
-//   },
-// });
-
-// export default store;
-
-// ****************************New Code below****************************
-
 import { configureStore } from "@reduxjs/toolkit";
 import artistsReducer from "./reducers/artistsReducer";
 import authReducer from "../store/authSlice";
