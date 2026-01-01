732ecdf97e2a34c7285fcfd9791bd3172b519623
Andre Wilkinson <andre_w3@yahoo.com>
2026-01-01 04:01:59 -0500

    refactor axiosInstance

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,104 +1,26 @@
+// src/utils/axiosInstance.js
 import axios from "axios";
 
+// Determine base URL: Priority 1 is .env variable, Priority 2 is Heroku, Priority 3 is local proxy
+const BASE_URL =
+  import.meta.env?.VITE_API_URL || "https://ninebyfourapi.herokuapp.com/api";
+
 const axiosInstance = axios.create({
-  // Use a relative URL for the API base.
-  // This will be prefixed to all requests and allows the Vite proxy
-  // to intercept them during development.
-  baseURL: "/api",
+  baseURL: BASE_URL,
 });
 
-// --- FIX STARTS HERE ---
-// Add a request interceptor to include the token in all requests.
 axiosInstance.interceptors.request.use(
   (config) => {
-    // Get the authentication token from localStorage.
-    // Make sure the key 'authToken' matches what you use when saving the token on login.
-    const token = localStorage.getItem("authToken");
+    // CRITICAL: We changed this to 'token' to match your authSlice logic
+    const token = localStorage.getItem("token");
 
-    // If a token exists, add it to the Authorization header.
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
 
     return config;
   },
-  (error) => {
-    // Do something with request error
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
-// --- FIX ENDS HERE ---
-
-// export default axiosInstance;
 
 export default axiosInstance;
-
-// *************************NEW CODE**************************
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   // Point to your live Heroku backend
-//   baseURL: "https://ninebyfourapi.herokuapp.com/api",
-// });
-
-// // Add a request interceptor to include the token in all requests.
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     // Get the authentication token from localStorage.
-//     const token = localStorage.getItem("authToken");
-
-//     // If a token exists, add it to the Authorization header.
-//     if (token) {
-//       config.headers["Authorization"] = `Bearer ${token}`;
-//     }
-
-//     return config;
-//   },
-//   (error) => {
-//     // Do something with request error
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default axiosInstance;
-// VITE_API_URL=https://ninebyfourapi.herokuapp.com/api
-
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   // Point to your live Heroku backend
-//   baseURL:
-//     import.meta.env.VITE_API_URL || "https://ninebyfourapi.herokuapp.com/api",
-// });
-
-// // Add a request interceptor to include the token in all requests.
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     // Get the authentication token from localStorage.
-//     const token = localStorage.getItem("authToken");
-
-//     // If a token exists, add it to the Authorization header.
-//     if (token) {
-//       config.headers["Authorization"] = `Bearer ${token}`;
-//     }
-
-//     return config;
-//   },
-//   (error) => {
-//     // Do something with request error
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default axiosInstance;
-// ```
-
-// **Now try logging in with:**
-// - Username: `andrefullstack`
-// - Password: `Test1234` (or whatever password you just set in Heroku)
-
-// This should work now because it's actually connecting to your Heroku backend! 🚀
-
-// If you want to use environment variables, create a `.env` file in your frontend root:
-// ```
-// VITE_API_URL=https://ninebyfourapi.herokuapp.com/api
